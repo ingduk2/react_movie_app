9fce49818279fdf3a4d4c3b46275458a798f49a3
ingduk2 <>
2019-10-14 06:08:30 +0900

    Updates

---
+++ precache-manifest.ad5a3042167428b7f05c58899a2a2ba7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a40fd5f3cbe9c6e6279ab84b03d4ec81",
+    "url": "/react_movie_app/index.html"
+  },
+  {
+    "revision": "ea095e984d4e75ed849a",
+    "url": "/react_movie_app/static/css/main.6fe6c1ef.chunk.css"
+  },
+  {
+    "revision": "79faa496ccda04b1599e",
+    "url": "/react_movie_app/static/js/2.650bcaf7.chunk.js"
+  },
+  {
+    "revision": "ea095e984d4e75ed849a",
+    "url": "/react_movie_app/static/js/main.62a14df2.chunk.js"
+  },
+  {
+    "revision": "58b971e95f343e29db45",
+    "url": "/react_movie_app/static/js/runtime-main.b313cadc.js"
+  }
+]);
